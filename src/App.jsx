f840879afd44be65dd9943ca3df008be2e3f9ec9
YuZhang <yuzhangpaul@gmail.com>
2025-07-03 09:53:02 +0900

    Update project to include React Router for navigation. Added routes for Dashboard, Settings, Tasks, and Timer pages, along with a NotFoundPage component. Updated package.json and package-lock.json to include react-router-dom as a dependency. Modified main entry point to wrap App component with BrowserRouter for routing support.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import NotFoundPage from './pages/NotFoundPage'
+import Dashboard from './pages/Dashboard'
+import Settings from './pages/Settings'
+import Timer from './pages/Timer'
+import Tasks from "./pages/Tasks";
 
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path='/' element={<Dashboard/>}></Route>
+      <Route path='/settings' element={<Settings/>}></Route>
+      <Route path='/tasks' element={<Tasks/>}></Route>
+      <Route path='/timer' element={<Timer/>}></Route>
+      <Route path='*' element={<NotFoundPage/>}></Route>
+    </Routes>
+    <div>
+      <Link to={"/"}>HomePage</Link>
+      <Link to={"/settings"}>Settings</Link>
+      <Link to={"/tasks"}>Tasks</Link>
+      <Link to={"/timer"}>Timer</Link>
+    </div>
     </>
   )
 }
